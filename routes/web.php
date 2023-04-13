2203eedd3b1485568578eb4b78bef25df6ca53c0
hermey <herman@malcsdev.com>
2023-04-13 14:59:45 -0400

    employee/model/controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/employees',[EmployeeController::class,'index']);
+route::get('/employees/{id}',[EmployeeController::class,'show']);
